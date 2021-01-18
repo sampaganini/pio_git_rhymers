a3bd07c369eea7ca4635e1a8283cacafa340daac
kama <k.kulis@1liceum.info>
2021-01-18 02:00:14 +0100

    1.5 podpunkt 1

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,7 +4,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	private int totalRejected = 0;
 
-	private int reportRejected() {
+	public int reportRejected() {
 		return getTotalRejected();
 	}
 
