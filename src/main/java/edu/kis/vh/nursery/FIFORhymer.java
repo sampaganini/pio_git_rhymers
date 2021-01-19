29ecce074d57a534532fe4e65772f7dbb7a09c0c
kama <k.kulis@1liceum.info>
2021-01-19 23:53:54 +0100

    1.5 podpunkt 1.6

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,8 +2,9 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+	/** creating new defaultCountingOutRhymer cause we extends him **/
 	private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-	
+	/** override orginal countOut class from defaultcountingoutrhymes**/
 	@Override
 	public int countOut() {
 		while (!callCheck())
